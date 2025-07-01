493d34f484ac2044ad63debcc79953c2b1d66571
naluRMeira <ana.ribeiro.meira@escola.pr.gov.br>
2025-07-01 17:10:34 -0300

    taattataattaattatatatatatatattatatatatatatt

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao{
+    'PROGRAMAÇÃO'
+    'O que é JavaScript?'
+    'O javaScript é uma linguagem de programação.'
+}
+
+criaCartao{
+    'formula1'
+    'Quem é o maior campeão da Fórmula 1?'
+    'Michael Schumacher e Lewis Hamilton, ambos com 7 títulos.'
+}
+
+criaCartao{
+    'formula1'
+    'Qual equipe tem mais títulos de construtores?'
+    'Ferrari, com 16 títulos.'
+}
+
+criaCartao{
+    'formula1'
+    'Quem venceu o primeiro GP de Fórmula 1?'
+    'Giuseppe Farina, no GP da Itália de 1950.'
+}
+
+criaCartao{
+    'formula1'
+    'Qual o nome do circuito mais longo da Fórmula 1?'
+    'Circuito de Spa-Francorchamps, com 7.004 km.'
+}
